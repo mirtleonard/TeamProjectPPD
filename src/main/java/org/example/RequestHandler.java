957f0064443c7058a959e3326def0e3d151fecfa
mirtleonard <leonardmirt26@gmail.com>
2024-01-11 18:02:57 +0200

    new refactoring

--- src/main/java/org/example/RequestHandler.java
+++ src/main/java/org/example/RequestHandler.java
@@ -14,15 +14,14 @@ import java.util.concurrent.*;
 // this handler should be different for client and server
 public class ConsoleRequestHandler implements IRequestHandler {
     private static final Logger logger = LoggerFactory.getLogger(ConsoleRequestHandler.class);
-    private final ExecutorService executorService;
     private final CountDownLatch producerLatch;
     private final CustomQueue<Future> finalRankingResponseTasks;
     private final CustomQueue<Future> responseTasks;
     private final CustomQueue<Participant> processedData;
     private final ConcurrentLinkedList linkedList;
 
-    public ConsoleRequestHandler(ConcurrentLinkedList linkedList, ExecutorService clientDataExecutorService, CustomQueue processedData, CustomQueue responseTasks, CustomQueue finalRankingResponseTasks, CountDownLatch producerLatch) {
-        this.executorService = clientDataExecutorService;
+
+    public ConsoleRequestHandler(ConcurrentLinkedList linkedList, CustomQueue processedData, CustomQueue responseTasks, CustomQueue finalRankingResponseTasks, CountDownLatch producerLatch) {
         this.processedData = processedData;
         this.producerLatch = producerLatch;
         this.linkedList = linkedList;
@@ -38,7 +37,7 @@ public class ConsoleRequestHandler implements IRequestHandler {
         if (type.contains("sending-data")) {
             Object body = request.get("body");
             String country = header.get("Country").toString();
-            executorService.submit(new ProducerThread(processedData, country, body));
+            addDataInQueue(country, body);
             connection.terminate();
         } else if (type.contains("get-ranking")) {
             logger.info("Received request for ranking");
@@ -58,30 +57,16 @@ public class ConsoleRequestHandler implements IRequestHandler {
         }
     }
 
-    class ProducerThread implements Runnable {
-        private final CustomQueue processedData;
-        private String country;
-        private Object data;
+    public void addDataInQueue(String country, Object data) {
+        logger.info("Processing data for country {}", country);
 
-        public ProducerThread(CustomQueue processedData, String country , Object data) {
-            this.processedData = processedData;
-            this.country = country;
-            this.data = data;
-        }
+        JSONArray rawParticipants = (JSONArray) data;
 
-        @Override
-        public void run() {
-            logger.info("Processing data for country {}", country);
-
-            JSONArray rawParticipants = (JSONArray) data;
-
-            for (Object participant : rawParticipants) {
-                String[] participantData = participant.toString().split(" ");
-                int id = Integer.parseInt(participantData[0]);
-                int score = Integer.parseInt(participantData[1]);
-                processedData.add(new Participant(id, score, country));
-            }
+        for (Object participant : rawParticipants) {
+            String[] participantData = participant.toString().split(" ");
+            int id = Integer.parseInt(participantData[0]);
+            int score = Integer.parseInt(participantData[1]);
+            processedData.add(new Participant(id, score, country));
         }
     }
-
 }
